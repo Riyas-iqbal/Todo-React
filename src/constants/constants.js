100b41373060aabff724d067102f89709b807513
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-01-09 16:13:09 +0530

    env setup

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'https://api.themoviedb.org/3'
-export const API_KEY = process.env.REACT_APP_API_KEY
+export const API_KEY =  process.env.REACT_APP_API_KEY
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
 export const imageUrlLow = 'https://image.tmdb.org/t/p/w500'
 
